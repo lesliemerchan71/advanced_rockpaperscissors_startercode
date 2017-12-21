f37a45bae41323760453126d6b6aee3e2231d054
Leslie Merchan <lesliemerchan71@gmail.com>
2017-12-21 23:11:02 +0000

    debugged some errors

--- js/script.js
+++ js/script.js
@@ -4,8 +4,67 @@
 
 //GLOBAL VARIABLES
 
+var random = "";
+var choices = ["rock", "paper", "scissors"];
+var userChoice="";
+var computerChoice="";
+var winner="";
+
 
 
 
 // DOCUMENT READY FUNCTION BELOW
 
+$(document).ready (function(){
+        
+function randomizer(){
+        random = Math.floor(Math.random()*3);
+}        
+        
+function ComputerChoice(){
+        randomizer();
+        var selection = choices[random];
+        computerChoice = selection; 
+        console.log(computerChoice);
+}
+function UserChoice(){
+        var selection = $('.input').val();
+        userChoice = selection; 
+        console.log(userChoice);
+}
+function reset(){
+        userChoice = "";
+        computerChoice = "";
+}
+
+
+$('button').click(function(){
+             reset();
+             ComputerChoice();
+             UserChoice();
+            if (computerChoice == 'rock' && userChoice == 'paper' || computerChoice == 'paper' && userChoice == 'scissors'|| computerChoice == 'scissors' && userChoice == 'rock'){
+                    alert('You win!');
+                    $("#You").append("<br /> You chose " +userChoice+ "   ");
+                    $("#You").append("Computer chose " +computerChoice+ "  W");
+                    
+            } else {
+            if (computerChoice == 'rock' && userChoice == 'scissors' || computerChoice == 'paper' && userChoice == 'rock'|| computerChoice == 'scissors' && userChoice == 'paper'){
+                    alert('You LOSE!');
+                    $("#You").append("<br /> You chose " +userChoice+ "   ");
+                    $("#You").append("Computer chose " +computerChoice+ "  L");
+            } else {
+            if (computerChoice == 'rock' && userChoice == 'rock' || computerChoice == 'paper' && userChoice == 'paper'|| computerChoice == 'scissors' && userChoice == 'scissors'){
+                    alert('You TIE!');
+                    $("#You").append("<br /> You chose " +userChoice+ "   ");
+
+$("#You").append("Computer chose " +computerChoice+ "  T");
+            } else {
+            if (userChoice != 'rock' || userChoice != 'paper' || userChoice != 'scissors') {
+                    alert("Try doing it right this time...");
+            }  
+            }
+            }   
+            }
+    });
+        
+});
